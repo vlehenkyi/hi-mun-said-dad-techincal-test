cc603375035a7d54c0da3c0892e3b7ce6ab37db0
Volodymyr Lehenkyi <volodymyrlehenkyi@Volodymyrs-MBP.sweetsoft.com.ua>
2020-08-20 18:54:34 +0300

    Add weather module to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
-import { weatherByLatLong } from "./config/endpoints";
-import options from "./config/options";
+import Weather from "./components/Weather";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
 }
 
 export default App;
